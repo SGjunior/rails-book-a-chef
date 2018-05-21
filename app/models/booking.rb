fd9dc736f2e65585b8654d4246c109b9117c4b9e
jack gaarkeuken <jack.gaarkeuken@gmail.com>
2018-05-21 15:42:39 -0400

    custom validations bookings user

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -7,4 +7,20 @@ class Booking < ApplicationRecord
   validates :reservation_date, presence: true, uniqueness: true
   validates :reservation_time, presence: true
   validates :guests_count, presence: true # TODO : add validation to be smaller then listing.max_person and bigger then listing.min_person
+  validates :booking_limits
 end
+
+
+
+
+
+  private
+
+  def booking_limits
+    if guests_count > 10
+      errors.add(:guests_count, "can't have more than 10 guests")
+    elsif volume < 2
+      errors.add(:guests_count, "can't have less than 2 guest"
+    end
+  end
+end)
