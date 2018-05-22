311db85e7329dd98ed90060ae04a80590c39dc5e
Simon Guertin <sim.guertin@gmail.com>
2018-05-21 20:23:21 -0400

    added listing new form

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -11,9 +11,17 @@ class ListingsController < ApplicationController
   end
 
   def new
+    @listing = Listing.new
   end
 
   def create
+    @listing = Listing.new(listing_params)
+
+    if @listing.save!
+      redirect_to listing_path(@listing.id)
+    else
+      render :new
+    end
   end
 
   def update
@@ -28,6 +36,10 @@ class ListingsController < ApplicationController
   private
 
   # TODO - determine if booking should be instantiated here and called in to show method
+
+  def listing_params
+    params.require(:listing).permit(:description, :cuisine, :min_party_size, :max_party_size, :price_per_person, :user_id, :lng, :lat)
+  end
 
   def set_booking
     @booking = Booking.new
