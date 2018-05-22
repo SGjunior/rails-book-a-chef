d144d920f0c6ba63c6999ef711ab3ea3fe4ddf7d
Adam Frej <awfrej@googlemail.com>
2018-05-22 17:40:18 -0400

    show UI sticky form complete navbar error fixed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,19 +2,19 @@
 //= require_tree .
 //= require jquery
 
-window.onscroll = myFunction;
+// window.onscroll = myFunction;
 
-// Get the navbar
-var navbar = document.getElementById("sticky-form");
+// // Get the navbar
+// var navbar = document.getElementById("sticky-form");
 
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+// // Get the offset position of the navbar
+// var sticky = navbar.offsetTop;
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
+// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,16 +4,16 @@ import "bootstrap";
 window.onscroll = function() {myFunction()};
 
 // Get the navbar
-// var stickyForm = document.getElementById("sticky-form");
+var stickyForm = document.getElementById("sticky-form");
 
-// // Get the offset position of the navbar
-// var sticky = stickyForm.offsetTop;
+// Get the offset position of the navbar
+var sticky = stickyForm.offsetTop;
 
-// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-// function myFunction() {
-//   if (window.pageYOffset >= sticky) {
-//     navbar.classList.add("sticky")
-//   } else {
-//     navbar.classList.remove("sticky");
-//   }
-// }
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    stickyForm.classList.add("sticky")
+  } else {
+    stickyForm.classList.remove("sticky");
+  }
+}
