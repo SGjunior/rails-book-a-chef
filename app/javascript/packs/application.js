40b456920b292c49dca4b72c2ab05444e778a8f9
Simon Guertin <sim.guertin@gmail.com>
2018-05-23 14:47:26 -0400

    Revert "Revert "Merge branch 'master' into adding-footer""
    This reverts commit 5002dcc52f899367c74cad308afda7eab246339d.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,12 @@
 import "bootstrap";
 
+import { autocomplete } from '../components/autocomplete';
+autocomplete();
+
+
+// todo : this should be in a different file
+
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
 
 // Get the navbar
 var stickyForm = document.getElementById("sticky-form");
@@ -9,6 +14,7 @@ var stickyForm = document.getElementById("sticky-form");
 // Get the offset position of the navbar
 if (stickyForm) {
   var sticky = stickyForm.offsetTop;
+  window.onscroll = function() {myFunction()};
 }
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
