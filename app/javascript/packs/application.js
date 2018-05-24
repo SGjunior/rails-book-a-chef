c199c241bb946a169078e534783b877cf9d096cd
Adam Frej <awfrej@googlemail.com>
2018-05-24 16:24:41 -0400

    show page layout fixed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,10 +10,10 @@ var stickyForm = document.getElementById("sticky-form");
 // Get the offset position of the navbar
 if (stickyForm) {
   var sticky = stickyForm.offsetTop;
-  window.onscroll = function() {myFunction()};
+  window.onscroll = function() {stickWhenScrolling()};
 }
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
+function stickWhenScrolling() {
   if (window.pageYOffset >= sticky) {
     stickyForm.classList.add("sticky")
   } else {
